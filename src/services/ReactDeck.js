7b008f5a7fbfd311e6e280c347501d67570ee065
Akoruu.Dev <akoruu.dev@gmail.com>
2022-09-30 19:14:37 -0300

    FIX [ReactDeck]: Update name object on ReactDeck.js

--- src/services/ReactDeck.js
+++ src/services/ReactDeck.js
@@ -1,35 +1,35 @@
 const ReactDeck = [
     {
         question: 'O que é JSX?',
-        answere: 'Uma extensão de linguagem do JavaScript'
+        answer: 'Uma extensão de linguagem do JavaScript'
     },
     {
         question: 'O React é __',
-        answere: 'uma biblioteca JavaScript para construção de interfaces'
+        answer: 'uma biblioteca JavaScript para construção de interfaces'
     },
     {
         question: 'Componentes devem iniciar com __',
-        answere: 'letra maiúscula'
+        answer: 'letra maiúscula'
     },
     {
         question: 'Podemos colocar __ dentro do JSX',
-        answere: 'expressões'
+        answer: 'expressões'
     },
     {
         question: 'O ReactDOM nos ajuda __',
-        answere: 'interagindo com a DOM para colocar componentes React na mesma'
+        answer: 'interagindo com a DOM para colocar componentes React na mesma'
     },
     {
         question: 'Usamos o npm para __',
-        answere: 'gerenciar os pacotes necessários e suas dependências'
+        answer: 'gerenciar os pacotes necessários e suas dependências'
     },
     {
         question: 'Usamos props para __',
-        answere: 'passar diferentes informações para componentes '
+        answer: 'passar diferentes informações para componentes '
     },
     {
         question: 'Usamos estado (state) para __',
-        answere: 'dizer para o React quais informações quando atualizadas devem renderizar a tela novamente'
+        answer: 'dizer para o React quais informações quando atualizadas devem renderizar a tela novamente'
     }
 ]
 
