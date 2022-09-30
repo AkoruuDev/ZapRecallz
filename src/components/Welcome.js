b5da64075027e5dd5fae2dfc26f3c2bcec0f6edd
Akoruu.Dev <akoruu.dev@gmail.com>
2022-09-30 00:43:46 -0300

    STYLE [Welcome]: Create Welcome page style using styled-components

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,11 +1,49 @@
+import styled from "styled-components"
+import logo from "../images/logo.png";
+
 export default function Welcome({ setStart }) {
     return(
-        <>
-            <>
-                <img src="" />
-                <h1>ZapRecall</h1>            
-            </>
-            <button onClick={() => setStart(true)}>Começar Recall</button>
-        </>
+        <Conteiner>
+            <div>
+                <img src={logo} />
+                <Title>ZapRecall</Title>            
+            </div>
+            <Button onClick={() => setStart(true)}>Começar Recall</Button>
+        </Conteiner>
     )
 }
+
+const Conteiner = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-color: #FB6B6B;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+`
+
+const Title = styled.h1`
+    margin: 20px 0;
+
+    font-family: 'Righteous', cursive;
+    font-size: 36px;
+    color: #FFFFFF;
+`
+
+const Button = styled.div`
+    padding: 20px 50px;
+    background-color: #FFFFFF;
+    border-radius: 5px;
+    border: 1px solid #D70900;
+
+    font-family: 'Recursive', sans-serif;
+    font-size: 18px;
+    color: #D70900;
+
+    &:hover {
+        cursor: pointer;
+        box-shadow: 0 4px 10px 4px #D90922;
+    }
+`
