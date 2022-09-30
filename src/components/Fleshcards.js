b6e3c9134853409c8578cd1fe6cd038a7eb85cb5
Akoruu.Dev <akoruu.dev@gmail.com>
2022-09-30 18:34:09 -0300

    STYLE [Viwer]: Create a styled components for organize and show on viwer

--- src/components/Fleshcards.js
+++ src/components/Fleshcards.js
@@ -1,5 +1,16 @@
+import styled from "styled-components"
+
 export default function Fleshcards() {
     return(
-        <></>
+        <Container>
+            
+        </Container>
     )
 }
+
+const Container = styled.div`
+    width: 80vw;
+    height: 100vh;
+    padding: 120px 0 0 0;
+    background-color: #FB6B6B;
+`
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,14 +1,24 @@
 import Top from "./Top";
 import Fleshcards from "./Fleshcards";
 import Footer from "./Footer";
+import styled from "styled-components";
 
 export default function Game() {
 
     return(
-        <>
+        <Container>
             <Top />
             <Fleshcards />
             <Footer />
-        </>
+        </Container>
     )
 }
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-color: #FB6B6B;
+
+    display: flex;
+    justify-content: center;
+`
